71e52c28f3028131f55c38b55419ca1e6c76329f
adityanegara <aditnegarabisnis@gmail.com>
2021-08-18 01:23:45 +0700

    Finish E2E Liking Restaurant

--- e2e/Liking_Restaurant.spec.js
+++ e2e/Liking_Restaurant.spec.js
@@ -1,5 +1,7 @@
 Feature('Liking Restaurant');
 
+const assert = require('assert');
+
 Before(({ I }) => {
   I.amOnPage('/#/favorite');
 });
@@ -9,7 +11,21 @@ Scenario('showing empty liked restaurant', ({ I }) => {
   I.see('No Restaurant Found...', '#no-favorite-found-warning');
 });
 
-Scenario('liking one restaurant', ({ I }) => {
+Scenario('liking one restaurant', async ({ I }) => {
   I.see('No Restaurant Found...', '#no-favorite-found-warning');
   I.amOnPage('/');
+  I.seeElement('.restaurant-name');
+  const firstRestaurant = locate('.restaurant-name').first();
+  const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
+  const firstRestaurantDetailLink = locate('.restaurant-detail-link').first();
+  const firstRestaurantHref = await I.grabTextFrom(firstRestaurantDetailLink);
+  console.log(firstRestaurantHref);
+  console.log(firstRestaurantName);
+  I.click(firstRestaurantHref);
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+  I.amOnPage('/#/favorite');
+  I.seeElement('.restaurant');
+  const likedRestaurantName = await I.grabTextFrom('.restaurant-name');
+  assert.strictEqual(firstRestaurantName, likedRestaurantName);
 });
