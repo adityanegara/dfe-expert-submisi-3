648edf1dd2b8c9913393cdc398e7f7fa0499f729
adityanegara <aditnegarabisnis@gmail.com>
2021-08-18 11:38:46 +0700

    Compress Hero Image

--- src/scripts/component/hero/HeroContainer.js
+++ src/scripts/component/hero/HeroContainer.js
@@ -8,16 +8,10 @@ class HeroContainer extends HTMLElement {
   }
 
   render() {
-    this.innerHTML = `<div  style="
-    width: 100%;
-    height: 500px;
-    background-image: linear-gradient(to bottom, rgba(245, 71, 72, 0.52), rgba(0, 0, 0, 0.73)),
-    url('${this.backgroundImage}');
+    this.innerHTML = `<div 
   " class="hero-container">
-        <h2 tabindex = "0" >${this.title}</h2>
-        <h3 tabindex = "0" >${this.description}</h3>
-        <button aria-label="Download Button">${this.buttonCaption}</button>
-        </div>`;
+        <img style="width:100%; height:100%" src="${this.backgroundImage}"></img>
+    </div>`;
   }
 }
 
