66b23ed31c6f7e2e6fa0fc2df74ea0faec07d864
adityanegara <aditnegarabisnis@gmail.com>
2021-08-18 12:13:57 +0700

    Implementing Responsive Image on Hero

--- src/scripts/component/hero/HeroContainer.js
+++ src/scripts/component/hero/HeroContainer.js
@@ -3,14 +3,21 @@ class HeroContainer extends HTMLElement {
     this.title = this.getAttribute('title') || null;
     this.description = this.getAttribute('description') || null;
     this.buttonCaption = this.getAttribute('button-caption') || null;
-    this.backgroundImage = this.getAttribute('background-image') || null;
+    this.backgroundImageLarge = this.getAttribute('background-image-large') || null;
+    this.backgroundImageSmall = this.getAttribute('background-image-small') || null;
     this.render();
   }
 
   render() {
-    this.innerHTML = `<div 
-  " class="hero-container">
-        <img style="width:100%; height:100%" src="${this.backgroundImage}"></img>
+    this.innerHTML = `
+    <div class="hero-container">
+      <picture>
+        <source media="(max-width: 600px)" 
+        style="width:100%; height:100%"
+        srcset="${this.backgroundImageSmall}">
+        <img style="width:100%; height:100%" src="${this.backgroundImageLarge}"></img>
+      </picture>
+     
     </div>`;
   }
 }
